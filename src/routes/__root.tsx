bf24aab4f3eee1dd8bdd163a3503d1a49bbbc23f
Michael <michael@talentbait.com>
2025-07-04 17:42:22 +0200

    rename

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { Navbar } from '../Components/Navbar'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Navbar } from "../components/Navbar";
 
 export const Route = createRootRoute({
   component: () => (
@@ -11,4 +11,4 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
